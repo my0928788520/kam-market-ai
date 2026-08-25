8d3ae9172b42f29f6c5c137478e8f759b8e5e1ca
my0928788520 <my0928788520@gmail.com>
2026-08-26 07:33:57 +0800

    test: expect futures paper action labels

--- tests/test_paper_trading_operator_presenter.py
+++ tests/test_paper_trading_operator_presenter.py
@@ -10,7 +10,7 @@ def test_presenter_is_deterministic_read_only_and_html_safe() -> None:
     value = PaperOrderProposalInput("p", "v", "TEST", PaperOrderProposalAction.BUY, PaperOrderProposalOrderType.MARKET, Decimal("1"), Decimal("100"), None, Decimal("90"), Decimal("110"), Decimal("0.5"), PaperOrderProposalRisk(PaperOrderProposalRiskStatus.ACCEPTABLE, "<safe>"), (PaperOrderProposalReason("R", "<script>", 0),), now, datetime(2026, 8, 13, 3, tzinfo=UTC), "hash")
     view = build_operator_presenter(build_paper_order_proposal(value))
     assert view.read_only and view.dry_run and not view.live_order_allowed and not view.broker_connected
-    assert view.title == "KAM 模擬交易操作台"
+    assert view.title == "KAM 期貨模擬交易操作台"
     assert view.proposal["instrument"] == "TEST"
-    assert view.proposal["action"] == "買進"
+    assert view.proposal["action"] == "多單進場"
     assert build_operator_presenter(build_paper_order_proposal(value)) == view
